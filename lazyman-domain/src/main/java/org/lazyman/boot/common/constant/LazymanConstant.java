e0bfb2b4e7285d5b1004b3405ca3ffacb8b27972
wanglong <710773064@qq.com>
2021-03-18 11:37:24 +0800

    init

--- lazyman-domain/src/main/java/org/lazyman/boot/common/constant/LazymanConstant.java
+++ lazyman-domain/src/main/java/org/lazyman/boot/common/constant/LazymanConstant.java
@@ -1,11 +1,14 @@
 package org.lazyman.boot.common.constant;
 
 public interface LazymanConstant {
-    String COUNTRY_CODE = "86";
     Long MENU_ROOT_ID = 0L;
     int KAPTCHA_VERIFYCODE_EXPIRE_SECONDS = 60;
+    String COUNTRY_CODE = "86";
     int SMS_VERIFYCODE_EXPIRE_SECONDS = 5 * 60;
 
+    /**
+     * 系统权限
+     */
     interface Permission {
         String ADMIN = "*:*:*";
         /**
@@ -29,26 +32,41 @@ public interface LazymanConstant {
         String SYSTEM_MENU_REMOVE = "system:menu:remove";
     }
 
+    /**
+     * redis缓存key
+     */
     interface RedisKey {
         String AUTH_USER_PREFIX = "auth:user:";
         String AUTH_KAPTCHA_PREFIX = "auth:kaptcha:";
         String AUTH_SMS_VERIFYCODE_PREFIX = "auth:sms:verifyCode:";
     }
 
+    /**
+     * redis队列
+     */
     interface RedisQueue {
         String QUEUE_SMS_SEND = "queue:sms:send";
     }
 
+    /**
+     * redis锁
+     */
     interface RedisLock {
-        String USER_PREFIX = "";
+        String APP_USER_PREFIX = "";
     }
 
+    /**
+     * 短信通道厂商
+     */
     interface SmsChannelVendor {
         String CHUANGLAN = "chuanglan";
         String NXCLOUD = "nxcloud";
         String XINYICHEN = "xinyichen";
     }
 
+    /**
+     * 短信模板业务分类
+     */
     interface SmsTemplateCategory {
         int SMS_VERIFY_CODE_TEMPLATE = 10;
     }
